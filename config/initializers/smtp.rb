d3efe73f1fd8b5ec8093de197b8292319d0bb9af
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-28 15:00:51 +0200

    heroku authentification sender

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: 'gmail.com',
-  user_name: 'manager.bold.poncelet@gmail.com',
-  password: 'TestBOLD5665',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
   authentication: :login,
   enable_starttls_auto: true
 }
