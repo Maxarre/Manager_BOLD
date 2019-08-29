8c8a50cd622a0b20a74df72446bb0d3d02994e13
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-08-29 14:57:50 +0200

    Archives

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,6 +26,7 @@ class TasksController < ApplicationController
 
   def complete
     @list = List.find(params[:id])
+    @list.archive = true
     @user = current_user
     @task = Task.find(params[:list_id])
     @task.complete = true
--- db/migrate/20190826013301_create_lists.rb
+++ db/migrate/20190826013301_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :name
+      t.boolean :archive, default: false
       t.references :user, foreign_key: true
 
       t.timestamps
